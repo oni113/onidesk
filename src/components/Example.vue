ab59c206c12253407778249e94cdf973ea7f77af
oni <miskw99@gmail.com>
2019-06-27 19:35:59 +0900

    first commit

---
+++ src/components/Example.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            header
+        </div>
+        <div class="panel-body">
+            content
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<script type="text/javascript">
+export default {
+    name : 'Example',
+    data() {
+        return {
+            msg : 'Basic panel example'
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/TodoPage.vue
@@ -0,0 +1,92 @@
+<template lang="html">
+    <div class="container">
+        <h2>Todo List</h2>
+        <div class="input-group" style="margin-bottom:10px;">
+            <input type="text" class="form-control" name="" value="" placeholder="할일을 입력하세요." v-model="name" v-on:keyup.enter="createTodo(name)">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button" name="button" @click="createTodo(name)">ADD</button>
+            </span>
+        </div>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="(todo, index) in todos">
+                {{todo.name}}
+                <div class="btn-group pull-right" style="font-size:12px; line-height:1;">
+                    <button class="btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button" name="button">더보기<span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#" @click="deleteTodo(todo.id)">DEL</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id : 0,
+            name : null,
+            todos : [
+                {
+                    id : 1,
+                    name : '청소'
+                },
+                {
+                    id : 2,
+                    name : '코딩'
+                },
+                {
+                    id : 3,
+                    name : '건프라'
+                }
+            ]
+        };
+    },
+    mounted() {
+        this.getTodos();
+        this.todos.forEach((todo, index) => {
+            if (this.id < todo.id) {
+                this.id = todo.id;
+            }
+        });
+    },
+    methods : {
+        getTodos() {
+            // TODO : 서버 REST GET 구현 (axios)
+            var vm = this;
+            this.$http.get('/todos')
+                .then((result) => {
+                    console.log(result);
+                    this.todos = result.data;
+                });
+        },
+        deleteTodo(id) {
+            console.log(id);
+            // TODO : 서버 REST DEL 구현 (axios)
+            this.todos.forEach((todo, index) => {
+                if (todo.id === id) {
+                    this.todos.splice(index, 1);
+                }
+            });
+        },
+        createTodo(name) {
+            // TODO : 서버 REST POST 구현 (axios)
+            if (name != null) {
+                this.id++;
+                var newTodo = {
+                    id : this.id,
+                    name : name
+                };
+                console.log(newTodo);
+                this.todos.push(newTodo);
+            }
+            this.name = null;
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
