6c63998b6c7cc3396a5bc0c32c7e7309a43eff17
oni <miskw99@gmail.com>
2019-07-12 20:23:19 +0900

    add modal form

---
+++ src/components/common/Modal.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            header
+        </div>
+        <div class="panel-body">
+            content
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<script type="text/javascript">
+export default {
+    data() {
+        return {
+            msg : 'Basic panel example'
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
